d698ae53b0950f7097b4f1868600dfffaaa54b64
MR.CyberHack <94603667+Gwjlk@users.noreply.github.com>
2021-11-18 15:59:26 +0700

    Add files via upload

---
+++ kalkulator-1.py
@@ -0,0 +1,84 @@
+#!/bin/python2
+
+import os
+
+os.system('clear')
+
+os.system("figlet kalkulator")
+print("       BY  MR.CyberHack")
+print('(+)===============================(+)')
+print('(1). penambahan')
+print('(2). perngurangan')
+print('(3). perkalian')
+print('(4). pembagian')
+print('(+)==============================(+)')
+pilih = input('pilihan : ')
+if pilih == 1:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 + angka_2
+  print 'hasil',total
+
+elif pilih == 2:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 - angka_2
+  print 'hasil',total
+
+elif pilih == 3:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 * angka_2
+  print 'hasil',total
+
+elif pilih == 4:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 / angka_2
+  print 'hasil',total
+#!/bin/python2
+
+import os
+
+os.system('clear')
+
+os.system("figlet kalkulator")
+print("       BY  MR.CyberHack")
+print('(+)===============================(+)')
+print('(1). penambahan')
+print('(2). perngurangan')
+print('(3). perkalian')
+print('(4). pembagian')
+print('(+)==============================(+)')
+pilih = input('pilihan : ')
+if pilih == 1:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 + angka_2
+  print 'hasil',total
+
+elif pilih == 2:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 - angka_2
+  print 'hasil',total
+
+elif pilih == 3:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 * angka_2
+  print 'hasil',total
+
+elif pilih == 4:
+  print
+  angka_1 = input('angka ke1 : ')
+  angka_2 = input('angka ke2 : ')
+  total = angka_1 / angka_2
+  print 'hasil',total
